96f5621464b200230900f2f44641844665ab1a76
Ross Blair <rblair33@gmail.com>
2018-05-07 19:10:28 -0400

    Naming and determines exclusions

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
-document.getElementById("variable-submit").addEventListener("click", function(e) {
+let variables = [];
+
+
+document.getElementById("variable-submit").addEventListener("click", function (e) {
 
   e.preventDefault();
 
   let name = document.getElementById("variable-name").value.trim();
   let number = document.getElementById("number").value.trim();
 
-  for (let i = 0; i < number; i++) {
-    console.log(`${i}: ${name}`);
+  createVariableObjects(name, 1, number);
+
+  console.log(variables);
+});
+
+function createVariableObjects(name, i, total) {
+
+  let dummyVariable = {
+    name: variableNamer(name, i),
+    exclusions: determineExclusions(name, i)
   }
 
-});
+  variables.push(dummyVariable);
+
+  if (i < total) {
+    return createVariableObjects(name, i + 1, total);
+
+  } else {
+
+    return false;
+  }
+
+
+}
+
+function variableNamer(name, i) {
+  if (i < 10) {
+    return `~${name}0${i}`;
+  } else {
+    return `~${name}${i}`;
+  }
+}
+
+function determineExclusions(name, i) {
+  let exclusions = [];
+
+  if (i === 1) {
+    return exclusions;
+  } else {
+    for (let j = 1; j < i; j++) {
+      exclusions.push(variableNamer(name, j));
+    }
+    return exclusions;
+  }
+
+}
